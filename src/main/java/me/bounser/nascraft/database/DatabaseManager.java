489c503d946ddd00ddccf8df145561320ee7c513
Eduardo12B <109172340+TheEternalDark@users.noreply.github.com>
2025-05-22 11:09:48 -0300

    Config.yml

--- src/main/java/me/bounser/nascraft/database/DatabaseManager.java
+++ src/main/java/me/bounser/nascraft/database/DatabaseManager.java
@@ -37,12 +37,7 @@ public class DatabaseManager {
                 );
                 break;
 
-            case REDIS:
-                database = new Redis(
-                        Config.getInstance().getHost(),
-                        Config.getInstance().getPort(),
-                        Config.getInstance().getPassword()
-                ); break;
+
         }
 
         database.connect();
